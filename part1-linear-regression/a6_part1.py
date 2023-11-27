a6a2208b07ff7fdd749d2a46da1cadc4eabb90c9
Jacob <jhtruxal@cps.edu>
2023-11-27 11:31:24 -0600

    worked on part 1

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -6,15 +6,22 @@ from sklearn.linear_model import LinearRegression
 data = pd.read_csv("part1-linear-regression/blood_pressure_data.csv")
 x = data["Age"].values
 y = data["Blood Pressure"].values
-
+plt.figure(figsize=(8,6)) 
+plt.scatter(x,y)
+plt.xlabel("Age (years)")
+plt.ylabel("Blood Pressure")
+plt.title("Blood Pressure by Age")
 # Use reshape to turn the x values into 2D arrays:
 x = x.reshape(-1,1)
 
 # Create the model
+model = LinearRegression().fit(x, y)
 
 # Find the coefficient, bias, and r squared values. 
 # Each should be a float and rounded to two decimal places. 
-
+coef = round(float(model.coef_), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(x, y)
 
 # Print out the linear equation and r squared value
 
@@ -22,3 +29,4 @@ x = x.reshape(-1,1)
 # Print out the prediction
 
 # Create the model in matplotlib and include the line of best fit
+plt.show()
--- part1-linear-regression/example3_blood_pressure.py
+++ part1-linear-regression/example3_blood_pressure.py
@@ -1,3 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
